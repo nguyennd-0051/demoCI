71d63909c98d309cfc5a00ae0bcf2675354f652c
nguyennd-0051 <nguyen.duy.nguyen@sun-asterisk.com>
2020-09-04 16:43:47 +0700

    Updates

---
+++ precache-manifest.6331943b57d2c714b3677dfbc7874d65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e761c1582265207f159a1128a1bae34",
+    "url": "/demoCI/index.html"
+  },
+  {
+    "revision": "e2af8dde168962c4f68b",
+    "url": "/demoCI/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b72f05ea49767784898e",
+    "url": "/demoCI/static/js/2.2ecc4c5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demoCI/static/js/2.2ecc4c5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2af8dde168962c4f68b",
+    "url": "/demoCI/static/js/main.a479b9db.chunk.js"
+  },
+  {
+    "revision": "8135105174cf63ca6bb7",
+    "url": "/demoCI/static/js/runtime-main.142b3f35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demoCI/static/media/logo.5d5d9eef.svg"
+  }
+]);
